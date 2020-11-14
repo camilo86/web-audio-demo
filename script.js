f053873ff82698cfe6572749dba11510cb9e27ce
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2020-11-14 00:04:06 -0500

    add static page with play/pause button

---
+++ script.js
@@ -0,0 +1,34 @@
+// Create context for audio manipulation
+const audioContext = new AudioContext();
+
+// Load sound from <audio> tag
+const audioElement = document.querySelector("audio");
+const track = audioContext.createMediaElementSource(audioElement);
+
+track.connect(audioContext.destination);
+
+// Handle Play/Pause button
+const playPauseButtonElement = document.getElementById("play-pause-btn");
+
+playPauseButtonElement.addEventListener(
+  "click",
+  function () {
+    if (audioContext.state === "suspended") {
+      audioContext.resume();
+    }
+
+    if (this.dataset.playing === "false") {
+      audioElement.play();
+      this.dataset.playing = "true";
+    } else {
+      audioElement.pause();
+      this.dataset.playing = "false";
+    }
+  },
+  false
+);
+
+// Handle end of sound file
+audioElement.addEventListener("ended", function () {
+  playPauseButtonElement.dataset.playing = "false";
+});
